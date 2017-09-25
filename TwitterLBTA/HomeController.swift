c5773bf795262218458d0b625feae38a23c6e33b
Darragh Blake <darragh.blake48@gmail.com>
2017-09-25 10:07:12 +0100

    Removed Storyboard

---
+++ TwitterLBTA/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeController.swift
+//  TwitterLBTA
+//
+//  Created by Darragh on 9/25/17.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+    }
+}
