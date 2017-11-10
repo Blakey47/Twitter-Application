671ebbdafb83de743dc42f8e0fb9dca46b24e1b3
Darragh Blake <darragh.blake48@gmail.com>
2017-11-10 18:33:20 +0000

    User MVC Implementation

---
+++ TwitterLBTA/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  TwitterLBTA
+//
+//  Created by Darragh on 11/10/17.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    let name: String
+    let username: String
+    let bioText: String
+    let profileImage: UIImage
+}
